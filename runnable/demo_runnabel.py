1b3ee7a2ba06d0dc0fc396264fd3c04337fb9911
blackwithwhitegreen <placement.related.email@gmail.com>
2025-06-19 21:33:53 +0530

    Add files via upload

---
+++ runnable/demo_runnabel.py
@@ -0,0 +1,30 @@
+import random
+
+class NakliLLM:
+    def __init__(self):
+        print("LLM Created")
+
+    def predict(self,prompt):
+        response_list = [
+            'Delhi is the capital of India.',
+            'IPL is a cricket Game',
+            'AI wil destroy JS one day.'
+        ]
+        return {'response':random.choice(response_list)}
+    
+class NakliPromptTemplate:
+    def __init__(self, template, input_variables):
+        self.template = template
+        self.input_variables = input_variables
+
+    def format(self, input_dict):
+        return self.template.format(**input_dict)
+    
+
+template = NakliPromptTemplate(
+    template='Write a {length} poem about {topic}',
+    input_variables=['length','topic']
+
+)
+
+template.format({'length':'short','topic':'india'})
